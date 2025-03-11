907bb359348ce7193ccd33b141cb40bef8136c60
Samuel PELSY-MOZIMANN <samuel.pelsy@groupegiraud.fr>
2025-03-11 10:03:34 +0100

    energiraud scan

--- energiraud-staffjuicer/src/routes/+page.svelte
+++ energiraud-staffjuicer/src/routes/+page.svelte
@@ -11,7 +11,7 @@
 	let success = $state(null);
 	let transactions = $state([])
 	let balanceValidity = $state(null);
-	let dash_message = $state("Scannez une carte pour accéder aux informations du compte.");
+	let dash_message = $state("Lancez le scan pour accéder aux informations du compte.");
 	let ndef = $state(null);
 
 	let isListening = $state(false);
@@ -19,7 +19,7 @@
 	let abortController = $state(null);
 
 	async function startListening() {
-		dash_message = "Scan en cours...";
+		dash_message = "Scan en cours, veuillez approcher la carte";
     if (!("NDEFReader" in window)) {
       dash_message = "Votre navigateur ne prend pas en charge le NFC";
 			rfid_id = "04:54:a9:7a:89:5d:80";
@@ -50,7 +50,7 @@
 	async function stopListening() {
 		abortController.abort();
 		isListening = false;
-		dash_message = "Scannez une carte pour accéder aux informations du compte.";
+		dash_message = "Lancez le scan pour accéder aux informations du compte.";
 	}
 
 	/**
