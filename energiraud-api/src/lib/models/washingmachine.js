043055d812a859a9e98065e8a9225252511ea0b5
Samuel PELSY-MOZIMANN <samuel.pelsy@groupegiraud.fr>
2025-03-12 11:57:01 +0100

    machines per location

--- energiraud-api/src/lib/models/washingmachine.js
+++ energiraud-api/src/lib/models/washingmachine.js
@@ -1,33 +1,33 @@
 import { db } from '$lib/database';
 
 async function getWashingMachines() {
-  const washingMachines = await db.washingMachine.findMany();
+  const washingMachines = await db.machine.findMany();
   return washingMachines;
 }
 
 async function getWashingMachineFromLocation(location) {
-  const washingMachine = await db.washingMachine.findMany({
+  const washingMachine = await db.machine.findMany({
     where: { location }
   });
   return washingMachine;
 }
 
 async function getWashingMachineById(id) {
-  const washingMachine = await db.washingMachine.findUnique({
+  const washingMachine = await db.machine.findUnique({
     where: { id }
   });
   return washingMachine;
 }
 
 async function getWashingMachineBySerialNumber(serialNumber) {  
-  const washingMachine = await db.washingMachine.findUnique({
+  const washingMachine = await db.machine.findUnique({
     where: { serial_number: serialNumber }
   });
   return washingMachine;
 }
 
 async function getWashingMachinesPaginated(page, take) {
-  const washingMachines = await db.washingMachine.findMany({
+  const washingMachines = await db.machine.findMany({
     skip: (page - 1) * take,
     take
   });
