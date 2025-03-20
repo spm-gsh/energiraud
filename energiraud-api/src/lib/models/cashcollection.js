34fa2067be2ba5307dc576e9bf7f3b553e4f9b71
Samuel PELSY-MOZIMANN <samuel.pelsy@groupegiraud.fr>
2025-03-20 15:12:21 +0100

    feat(energiraud)

--- energiraud-api/src/lib/models/cashcollection.js
+++ energiraud-api/src/lib/models/cashcollection.js
@@ -102,12 +102,7 @@ async function getCurrentAmount(location) {
     orderBy: { created_at: 'desc' },
     take: 1
   });
-  let last_date = lastCashCollection[0].created_at;
-
-  // Si aucune collecte
-  if (lastCashCollection.length === 0) {
-    last_date = new Date('1970-01-01');
-  }
+  let last_date = lastCashCollection[0].created_at || new Date('1970-01-01');
 
   // Récupérer tous les dépots depuis la dernière collecte
   const cashDeposits = await db.cashDeposit.findMany({
