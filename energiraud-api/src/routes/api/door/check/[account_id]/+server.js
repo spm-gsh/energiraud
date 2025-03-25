cff71980f14d3734a6124905a5758c8f3d57f090
Samuel PELSY-MOZIMANN <samuel.pelsy@groupegiraud.fr>
2025-03-25 15:20:51 +0100

    salutmoicestmichel

--- energiraud-api/src/routes/api/door/check/[account_id]/+server.js
+++ energiraud-api/src/routes/api/door/check/[account_id]/+server.js
@@ -27,7 +27,6 @@ export async function GET({ request, params }) {
 
   // CHECK LOCATION
   const userLocation = request.headers.get('X-User-Location');
-  console.log(userLocation);
   if (account.role.name === 'ADMIN' || account.role.name === 'SUPER_ADMIN') {
     return json({ statusCode: 200, data: account }, { status: 200 });
   }
